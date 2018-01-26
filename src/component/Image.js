4707096ecfb4cd430bd00248153f59fe47e8904a
wangzhonghuan <wangzhonghuan@360.net>
2018-01-26 09:35:31 +0800

    add brightness fiter function

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './Image.css'
 
 class Image extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    // addEffects(e) {
+    //     console.log(e.target);
+    //     console.log()
+    //     let value = this.props.filter/100;
+    //     e.target.style.filter = `brightness(${value})`;
+    // }
+    
     render() {
+        let value = this.props.filter/100;
+        console.log('this is the redux value', value);
         return (
             <div className="app-image-container">
-                <div className="app-image"></div>
+                <div style={{filter:`brightness(${value})`}} className="app-image"></div>
             </div>
         );
     }
 }
 
-export default Image;
+function mapStateToProps(state) {
+    return {
+        filter: state.filter
+    }
+}
+
+export default connect(mapStateToProps, null)(Image);
